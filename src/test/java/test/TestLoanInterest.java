000f48e51bfd851fa5cba38b861df804a0dac764
Yosri-Jemai <yosrii.jemaiii@gmail.com>
2024-07-30 21:15:47 +0100

    JUnit

---
+++ src/test/java/test/TestLoanInterest.java
@@ -0,0 +1,38 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import interfaces.ICreditOperation;
+import operations.CreditOperation;
+
+public class TestLoanInterest {
+	
+	private ICreditOperation operation;
+
+	@Before
+	public void setUp() throws Exception {
+		operation = new CreditOperation();
+	}
+	
+	// Success
+	@Test
+	public void testLoanInterest1() {
+		double result = operation.LoanInterest(200000, 4.5, 240);
+		double expected = 1265.2987;
+		
+		assertEquals(expected, result, 0.0001);
+	}
+	
+	// Failure
+	@Test
+	public void testLoanInterest2() {
+		double result = operation.LoanInterest(200000, 4.5, 240);
+		double expected = 1265.2987;
+		
+		assertEquals(expected, result, 0.00001);
+	}
+	
+}
